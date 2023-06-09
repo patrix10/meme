1ec93ba2b38df51b1efdcc6cb02b0c1c07540187
patrix10 <125147974+patrix10@users.noreply.github.com>
2023-06-09 14:44:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const generateMemeBtn = document.querySelector(
+    ".meme .M"
+  );
+  const memeImage = document.querySelector(".meme img");
+  const memeTitle = document.querySelector(".meme .A");
+  const memeAuthor = document.querySelector(".meme .B");
+  
+  const updateDetails = (url, title, author) => {
+    memeImage.setAttribute("src", url);
+    memeTitle.innerHTML = title;
+    memeAuthor.innerHTML = `Meme by: ${author}`;
+  };
+  
+  const generateMeme = () => {
+    fetch("https://meme-api.com/gimme/wholesomememes")
+      .then((response) => response.json())
+      .then((data) => {
+        updateDetails(data.url, data.title, data.author);
+      });
+  };
+  
+  generateMemeBtn.addEventListener("click", generateMeme);
+  
+  generateMeme();
